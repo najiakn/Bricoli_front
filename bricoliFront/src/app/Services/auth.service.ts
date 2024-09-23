fd311e9945a73a60014d8b700a9302ee2783dd15
najia <najiaknizi199@gmail.com>
2024-09-23 16:41:24 +0100

    modifier register prestataire

--- bricoliFront/src/app/Services/auth.service.ts
+++ bricoliFront/src/app/Services/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) {}
 
   register(request: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/registerAdmin`, request);
+    return this.http.post(`${this.apiUrl}/registerPrestataire`, request);
   }
 
 
